e9c0c787ea14d1d507a9837d63f0a953f8b25921
truth0600 <minji.cho@digipen.edu>
2021-06-01 16:28:03 +0900

    add bullet

--- sketch.js
+++ sketch.js
@@ -1,6 +1,8 @@
 let line_size = 100;
 let wall_health = 300;
 let game_mode;
+const MAIN_MENU = 0;
+const GAME_START = 1;
 
 function preload()
 {
@@ -28,18 +30,20 @@ function draw()
     background( 220 )
 
     clear();
-    if ( game_mode == 0 )
+    if ( game_mode == MAIN_MENU )
     {
         push();
         textSize( 21 );
         text( 'press enter to start', 20, 40 )
         pop();
     }
-    if ( game_mode == 1 )
+    if ( game_mode == GAME_START )
     {
         //draw alines and wall of game_background class
     day1_background.drawing_lines();
     day1_background.drawing_wall();
+    activateballgun();
+	print(bullet.length);
     //call zombies
     for ( let day1_count = 0; day1_count < 10; day1_count++ )
     {
@@ -70,6 +74,6 @@ function keyPressed()
 {
     if ( keyCode == ENTER )
     {
-        game_mode = 1;
+        game_mode = GAME_START;
     }
 }
