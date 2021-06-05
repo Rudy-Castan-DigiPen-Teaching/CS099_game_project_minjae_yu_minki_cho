16b4527e5805bc20f59c93c7da22a1f1869ce0ab
truth0600 <minji.cho@digipen.edu>
2021-06-05 10:44:25 +0900

    Update wall.js
    make hp-bar more easy to see

--- wall.js
+++ wall.js
@@ -22,12 +22,14 @@ class Wall
     wall_hp()
     {
         //draw wall_health_bar
-        push()
-        rectMode(CENTER)
+        push();
+        rectMode(CORNER);
         this.wall_health_bar = this.wall_health / 10;
-        fill('green')
-        rect(line_size / 2, line_size * 3 - 25, this.wall_health_bar, 5)
-        pop()
+        fill('red');
+        rect(line_size / 3, line_size * 3 - 25, 30, 5);
+        fill('green');
+        rect(line_size / 3, line_size * 3 - 25, this.wall_health_bar, 5);
+        pop();
 
         //If wall become 0 or less then game over.
         if(this.wall_health <= 0)
