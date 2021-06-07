5b185a3a9b97ce17484a105ceba34862b75680b8
truth0600 <minji.cho@digipen.edu>
2021-06-07 19:49:26 +0900

    Update crosshair.js
    change stroke

--- game_screen/crosshair.js
+++ game_screen/crosshair.js
@@ -1,6 +1,7 @@
 //mouse crosshair
 function crosshair()
 {
+    stroke(255);
     line( mouseX, mouseY, mouseX + 10, mouseY );
     line( mouseX, mouseY, mouseX - 10, mouseY );
     line( mouseX, mouseY, mouseX, mouseY + 10 );
