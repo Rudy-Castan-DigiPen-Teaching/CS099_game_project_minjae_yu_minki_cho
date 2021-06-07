a0bc7ac68e74288a508d6db38a7a62092d3a571f
truth0600 <minji.cho@digipen.edu>
2021-06-07 19:49:42 +0900

    apply

--- wall.js
+++ wall.js
@@ -1,3 +1,10 @@
+let wall_img;
+
+function preload_wall()
+{
+    wall_img = loadImage( 'assets/images/wall2.jpg' );
+}
+
 class Wall
 {
     constructor()
@@ -14,25 +21,26 @@ class Wall
     {
         rectMode( CENTER );
         fill( 'brown' );
-        rect( this.x, this.y, this.width, this.height );
+        imageMode( CENTER );
+        image( wall_img, this.x, this.y )
         this.wall_hp();
-        console.log(this.wall_health);
+        console.log( this.wall_health );
     }
 
     wall_hp()
     {
         //draw wall_health_bar
         push();
-        rectMode(CORNER);
+        rectMode( CORNER );
         this.wall_health_bar = this.wall_health / 10;
-        fill('red');
-        rect(line_size / 3, line_size * 3 - 25, 30, 5);
-        fill('green');
-        rect(line_size / 3, line_size * 3 - 25, this.wall_health_bar, 5);
+        fill( 'red' );
+        rect( line_size / 3, line_size * 3 - 25, 30, 5 );
+        fill( 'green' );
+        rect( line_size / 3, line_size * 3 - 25, this.wall_health_bar, 5 );
         pop();
 
         //If wall become 0 or less then game over.
-        if(this.wall_health <= 0)
+        if ( this.wall_health <= 0 )
         {
             game_mode = GAME_OVER;
         }
